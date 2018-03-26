65587f4df5564ca173471f2d34aa52a6bbc899d3
zhaoyunxing92 <2385585770@qq.com>
2018-03-26 17:23:57 +0800

    创建gradle项目

---
+++ src/main/java/com/sunny/todo/App.java
@@ -0,0 +1,19 @@
+package com.sunny.todo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * @author sunny
+ * @className App
+ * @date 2018-03-26 14:23
+ * @description:
+ */
+public class App {
+    static Logger log = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+       log.info("hello gradle");
+    }
+}
